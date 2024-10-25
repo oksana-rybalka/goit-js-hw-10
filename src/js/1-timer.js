226a4921b05e1ea564fcaa3bb791aa4c2e4a692e
oksana-rybalka <rybalka-oksana@outlook.com>
2024-10-25 20:23:28 +0300

    invalid-js-syntax

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -9,12 +9,40 @@ import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
 
+
+const dateSelectionField = document.querySelector("#datetime-picker");
+const btnStart = document.querySelector("#data-start");
+
+let userSelectedDate = null;
+let timerInterval = null;
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
-  },
-};
+    const selectedDate = selectedDates[0];
+    const currentDay = new Date();
+
+    if (selectedDate <= currentDate) {
+      iziToast.error({
+        title: 'Error',
+        message: 'Please choose a date in the future',
+      });
+      btnStart.disabled = true;
+    } else {
+      userSelectedDate = selectedDate;
+      btnStart = false;
+    }
+    },
+  };
+
+ 
+flatpickr("#datetime-picker", options);
+
+btnStart.addEventListener('clikc', () => {
+  btnStart.disabled = true;
+  dateSelectionField.disabled = true;
+}
+
