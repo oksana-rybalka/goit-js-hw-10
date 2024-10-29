9855487d269a979400332201db2c200d8e0bc137
oksana-rybalka <rybalka-oksana@outlook.com>
2024-10-29 14:18:53 +0200

    finally

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -4,36 +4,36 @@ import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
 
-const form = document.querySelector(".form")
+const form = document.querySelector(".form");
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
     const delay = parseInt(event.target.delay.value);
     const state = event.target.state.value;
-    const promise = new Promise((resolve, reject) =>
+    const promise = new Promise((resolve, reject) => {
         setTimeout(() => {
             if (state === 'fulfilled') {
                 resolve(delay);
             } else {
                 reject(delay);
             }
-        }, delay),
+        }, delay);
+    })
+    promise
+        .then((delay) => {
+            iziToast.success({
+                title: 'Ok',
+                message: `✅ Fulfilled promise in ${delay}ms`,
+            });
+        })
+        .catch((delay) => {
+            iziToast.error({
+                title: 'Error',
+                message: `❌ Rejected promise in ${delay}ms`
 
-        promise
-            .then((delay) => {
-                iziToast.success({
-                    title: 'Ok',
-                    message: `✅ Fulfilled promise in ${delay}ms`,
-                });
-            })
-            .catch((delay) => {
-                iziToast.error({
-                    title: 'Error',
-                    message: `❌ Rejected promise in ${delay}ms`
-
-                });
-            })
-    )
+            });
+        })
+    
 });
 
 
